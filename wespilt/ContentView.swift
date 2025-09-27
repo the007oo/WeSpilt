c7849f1989bb45bba26bf62d01192c3c0b7ede39
Phattarapong Yodwiset <phattarapong.yod@gmail.com>
2025-09-27 19:11:44 +0700

    first initial

--- wespilt/ContentView.swift
+++ wespilt/ContentView.swift
@@ -1,6 +1,6 @@
 //
 //  ContentView.swift
-//  wespilt
+//  WeSplit
 //
 //  Created by Phattarapong Yodwiset on 27/9/2568 BE.
 //
@@ -8,14 +8,58 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 0
+    @State private var tipPercentage = 20
+    
+    private var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+        
+        let tipValue = checkAmount / 100 * tipSelection
+        let grandTotal = checkAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        return amountPerPerson
+    }
+    
+    private var totalAmount: Double {
+        let tipSelection = Double(tipPercentage)
+        let tipValue = checkAmount / 100 * tipSelection
+        return checkAmount + tipValue
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            Form {
+                Section("Amount") {
+                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD")).keyboardType(.decimalPad)
+                }
+                
+                Section("How many tips do you want to leave?") {
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(0 ..< 101) {
+                            Text($0, format: .percent)
+                        }
+                    }.pickerStyle(.navigationLink)
+                }
+                
+            
+                Section("Total Amount") {
+                    Text(totalAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
+                
+                Picker("Number of people",selection: $numberOfPeople) {
+                    ForEach(2 ..< 99) {
+                        Text("\($0) People")
+                    }
+                }.pickerStyle(.navigationLink)
+                
+                Section("Amount per person") {
+                    Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
+                
+            }.navigationTitle("WeSpilt").navigationBarTitleDisplayMode(.inline)
         }
-        .padding()
     }
 }
 
