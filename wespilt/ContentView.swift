537352cd218b3b186a660efbac08589a0713267c
Phattarapong Yodwiset <phattarapong.yod@gmail.com>
2025-09-27 19:03:27 +0700

    Initial Commit

---
+++ wespilt/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  wespilt
+//
+//  Created by Phattarapong Yodwiset on 27/9/2568 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ wespilt/wespiltApp.swift
@@ -0,0 +1,17 @@
+//
+//  wespiltApp.swift
+//  wespilt
+//
+//  Created by Phattarapong Yodwiset on 27/9/2568 BE.
+//
+
+import SwiftUI
+
+@main
+struct wespiltApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
